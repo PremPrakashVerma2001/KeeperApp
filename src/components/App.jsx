2f59539d2b092ec67b3fb414df43d2dc15154e9d
Prem Verma <ppverma.verma12@gmail.com>
2023-03-24 22:02:38 +0530

    larned 'props' and use them

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,41 @@ import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
 
-
+function NoteFour(props){
+  return (
+      <div className="note">
+          <h1>{props.title.t_value}</h1>
+          <p>{props.content.cont_value}</p>
+          <p>{props.emoji.emj}</p>
+      </div>
+  );
+  }
 function App() {
+//passing varible through props
+  const t_value  = "T3";
+  const cont_value = "c3"; 
+  const emj  = "👽";
+
+//passing object through props
+
+var noteinfo4 = {
+   t_value  :"T4",
+   cont_value :"c4", 
+   emj : "😵‍💫"
+}
+
   return (
     <div>
      <Header />
-     <Note></Note>
+     {/* simple props */}
+     <Note title= "T1" content ="c1" emoji="😀" />
+     <Note title= "T2" content ="c2" emoji="😶‍🌫️"/>
+     {/* props through variable */}
+     <Note title= {t_value} content = {cont_value} emoji={emj}/>
+
+
+    {/* props through objects */}
+     <NoteFour title= {noteinfo4} content ={noteinfo4} emoji={noteinfo4}/>
      <Footer/>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,9 @@
-function Note(){
+function Note(props){
 return (
     <div className="note">
-        <h1>title</h1>
-        <p>content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
+        <p>{props.emoji}</p>
     </div>
 );
 }
